5f93abe0438c60978b299f849e76271415a3ef38
erik-bobinski <ebobinski0@gmail.com>
2025-06-01 12:02:25 -0400

    implemented the updateInvoice Server Action: Create a form with pre-filled data, extract and validate form input data, insert data into DB, revalidate client-cache for invoices route and redirect user to invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,5 @@
 "use server";
 
-//TODO: finished Ch.12 starting from 'Updating an invoice'
-
 import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
@@ -18,6 +16,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -43,3 +42,22 @@ export async function createInvoice(formData: FormData) {
 
   redirect("/dashboard/invoices");
 }
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status")
+  });
+  const amountInCents = amount * 100;
+
+  await sql`
+  UPDATE invoices
+  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+  WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+//TODO: Continue Ch.12 at 'Deleting an invoice'
